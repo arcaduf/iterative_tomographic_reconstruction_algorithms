6c299f740f3c1a3802fc0842b413f969a6497ab7
arcaduf <arcusfil@gmail.com>
2017-01-09 16:30:18 +0100

    FIX: removed -p from all tests

--- scripts/test_reconstr_sir_3d_01.py
+++ scripts/test_reconstr_sir_3d_01.py
@@ -7,6 +7,6 @@ from __future__ import division , print_function
 import os
 
 os.chdir( '../algorithms' )
-command = 'python -W ignore sir.py -a em -Di ../data/sl3d/sl3d_128_sino_ang0200/ -Do ../data/sl3d/sl3d_128_reco_ang0200/ -pr grid-pswf -hc 10.0 -n 10 -p'
+command = 'python -W ignore sir.py -a em -Di ../data/sl3d/sl3d_128_sino_ang0200/ -Do ../data/sl3d/sl3d_128_reco_ang0200/ -pr grid-pswf -hc 10.0 -n 10'
 print( '\n\nCommand line:\n', command )
 os.system( command )
--- scripts/test_reconstr_sir_3d_02.py
+++ scripts/test_reconstr_sir_3d_02.py
@@ -7,6 +7,6 @@ from __future__ import division , print_function
 import os
 
 os.chdir( '../algorithms' )
-command = 'python -W ignore sir.py -a sps -Di ../data/sl3d/sl3d_128_sino_ang0200/ -Do ../data/sl3d/sl3d_128_reco_ang0200/ -pr grid-pswf -r huber -hc 0.1 -n 10 -p'
+command = 'python -W ignore sir.py -a sps -Di ../data/sl3d/sl3d_128_sino_ang0200/ -Do ../data/sl3d/sl3d_128_reco_ang0200/ -pr grid-pswf -r huber -hc 0.1 -n 10'
 print( '\n\nCommand line:\n', command )
 os.system( command )
